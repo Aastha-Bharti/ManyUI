75bc60aa3125b3fedd67ab44acb5b7efc22f9514
Aastha-Bharti <asbharti27@gmail.com>
2026-03-26 23:46:37 +0530

    fixes

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/section/Hero";
 import Features from "@/section/Features";
 import ComponentPreview from "@/section/ComponentPreview";
-import CTA from "@/section/CTA";
 import Footer from "@/section/Footer";
 
 export default function Home() {
